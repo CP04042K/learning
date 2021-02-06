9690284f051411f7590c737afeead0528cc3a695
Pham Chanh <lemon.chanh@hotmail.com>
2021-02-06 18:20:47 +0700

    Add mouse.js

---
+++ mouse.js
@@ -0,0 +1,8 @@
+function Mouse(weight, color, isStomachEmpty, isSleeping) {
+	this.weight = weight;
+	this.color = color;
+	this.isStomachEmpty = isStomachEmpty;
+	this.isSleeping = isSleeping;
+}
+
+module.exports = Mouse;
