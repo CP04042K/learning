ee8adfd9fef6c24b54fffb41219e263ac28bc5fa
Pham Chanh <lemon.chanh@hotmail.com>
2021-02-07 22:01:00 +0700

    afadfasdg

--- main.js
+++ main.js
@@ -5,4 +5,4 @@ var Horse = require("./horse");
 var mouse = new Mouse(2, "gray", true, true);
 var cat = new Cat("tom");
 console.log(horse.name);
-console.log(mouse.color);
+console.log(mouse.weight);
