51074f75db781de980b2e0190cceea2cd5aaa11e
Pham Chanh <lemon.chanh@hotmail.com>
2021-02-07 20:20:42 +0700

    fix

--- horse.js
+++ horse.js
@@ -3,10 +3,7 @@ function Horse(name, speed) {
     this.speed = speed;
 }
 
-<<<<<<< HEAD
 module.exports = Horse; 
 console.log("asfasfasfasf");
-=======
 module.exports = Horse;
 console.log('aa');
->>>>>>> 82767c2 (aaa)
